d171cb2160000dd461cf88de2444584a69811c7f
fabiosvd <fabiosalvador85@yahoo.com.br>
2021-12-24 13:46:33 -0300

    should not be able to a non admin user get list of all users :heavy_check_mark:

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,6 +9,12 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
+    const user = this.usersRepository.findById(user_id)
+
+    if (!user.admin) {
+      throw new Error("Only admins have access to the list.");
+    }
+
     const users = this.usersRepository.list();
     return users
   }
