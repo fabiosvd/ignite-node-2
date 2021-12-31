57cd4fe820f1d34561be41531a3c4a25eb8e81da
fabiosvd <fabiosalvador85@yahoo.com.br>
2021-12-31 10:03:31 -0300

    should not be able to a non admin user get list of all users :heavy_check_mark:

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -8,13 +8,15 @@ class ListAllUsersController {
   handle(request: Request, response: Response): Response {
     const { user_id } = request.headers;
 
-
+    try {
       const all = this.listAllUsersUseCase.execute({
         user_id: String(user_id),
       });
 
       return response.json(all);
-
+    } catch (err) {
+      return response.status(400).json({ error: err });
+    }
   }
 }
 
