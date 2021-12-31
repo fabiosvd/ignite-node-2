9c869f1f7dd6031f141aa639774e6c225370b038
fabiosvd <fabiosalvador85@yahoo.com.br>
2021-12-31 10:05:36 -0300

    should not be able to show profile of a non existing user :heavy_check_mark:

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -8,11 +8,13 @@ class ShowUserProfileController {
   handle(request: Request, response: Response): Response {
     const { user_id } = request.params;
 
-
+    try {
       const user = this.showUserProfileUseCase.execute({ user_id });
 
       return response.json(user);
-
+    } catch (err) {
+      return response.status(404).json({ error: err });
+    }
   }
 }
 
